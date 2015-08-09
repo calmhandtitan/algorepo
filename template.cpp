3a153e160bdf3b2feaa18f25d8de5a2549806da8
Chandan Mittal <chandan.iiitdmj@gmail.com>
2015-08-10 01:57:12 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,10 +1,10 @@
-//THEM THAT STICK TO IT OUT, ARE THEM THAT WIN
-//The closer you think you're, the less you will actually see.
+//amazing takes time, legendary requires patience
 #include "bits/stdc++.h"
 #define sd(n) scanf("%d", &(n))
 #define rep(i, x, n) for (int i = x, _n = (n); i < _n; ++i)
-#define repV(i, v) for (i = v.begin(); i != v.end(); i++)
+#define repi(i, a) for(typeof((a).begin()) i = (a).begin(), _##i = (a).end(); i != _##i; ++i)
 #define SZ(c) (int)(c).size()
+#define pra(v, n) rep(i, 0, n) cout << v[i] << " "; cout << endl;
 #define lcm(a,b) (a*(b/__gcd(a,b)))
 #define VI vector<int>
 #define all(c) (c).begin(), (c).end()
@@ -16,16 +16,22 @@
 #define S second
 #define mp make_pair
 #define lli long long int
+#define llu unsigned long long
 #define CLR(p) memset(p, 0, sizeof(p))
 #define SET(p) memset(p, -1, sizeof(p))
 #define INF 0x3f3f3f3f
+#define pi 3.14159265358979
+#define debug 0
 using namespace std;
 
 const int MOD = 1e9+7;
+const int eps = -1e6;
 const int MAX = 100010;
+
 
 int main()
 {
-
+	ios_base::sync_with_stdio(0);
+	
     return 0;
 }    
